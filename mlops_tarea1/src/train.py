593fea3bc28a6a593be401ce3f0d62c53d7452c2
joseovalle_a01150742 <113394492+joseovalletecmx@users.noreply.github.com>
2024-11-15 10:13:02 -0600

    adding shapely values to code and saving img as artifact

--- mlops_tarea1/src/train.py
+++ mlops_tarea1/src/train.py
@@ -15,6 +15,7 @@ from sklearn.metrics import f1_score
 import argparse
 import joblib
 import mlflow
+import shap
 
 def train_model(config_path: Text) -> None:
     with open(config_path) as conf_file:
@@ -29,12 +30,16 @@ def train_model(config_path: Text) -> None:
     y_train = np.array(y_train).flatten()
     y_test = np.array(y_test).flatten()
 
+
+    print(X_train.info())
+
 # Initialize classifier
     model = LogisticRegression()
     model.fit(X_train, np.array(y_train).flatten())
     y_pred = model.predict(X_test)
+
+    # calculate evaluation metris
     accuracy = accuracy_score(y_test, y_pred)
-    # confusion = confusion_matrix(y_test, y_pred)
     precision = precision_score(y_test, y_pred)
     recall = recall_score(y_test, y_pred)
     f1 = f1_score(y_test, y_pred)
@@ -44,19 +49,34 @@ def train_model(config_path: Text) -> None:
     print("Classification Report:")
     print(classification_report(y_test, y_pred))
 
+
+    # SHAP analysis
+    k = len(X_train)
+    background_sampled = shap.sample(X_train, k)
+
+    # Calculate SHAP values using the KernelExplainer (logistic regression is linear)
+    explainer = shap.Explainer(model, background_sampled)
+    shap_values = explainer(X_test)
+
+    # Visualize SHAP results
+    shap_bar_plot_path = "shap_bar_plot.png"
+    shap.plots.bar(shap_values) # this one works too
+    plt.savefig(shap_bar_plot_path)
+    plt.close()
+
     mlflow.set_tracking_uri(config['mlflow']['tracking_uri'])
     mlflow.set_experiment(f'users/appendicitis_model')
 
     model_path = config['data_model']['model_path']
     joblib.dump(model, model_path)
     
-
     mlflow.start_run()
     mlflow.sklearn.log_model(model, "model", registered_model_name="sk-learn-logistic_regression-reg-model")
     mlflow.log_metric('accuracy', accuracy)
     mlflow.log_metric('precision_score', precision)
     mlflow.log_metric('recall_score', recall)
     mlflow.log_metric('f1_score', f1)
+    mlflow.log_artifact(shap_bar_plot_path, "explainability_artifacts")
     mlflow.end_run()
 
 if __name__ == '__main__':
